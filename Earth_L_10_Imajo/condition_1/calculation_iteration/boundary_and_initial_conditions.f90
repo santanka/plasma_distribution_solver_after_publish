f94a9e175f2e3672805c1920df79582b66075a5e
santanka <mkrock1062@gmail.com>
2023-10-13 19:57:05 +0900

    case1の条件の調整

--- Earth_L_10_Imajo/condition_1/calculation_iteration/boundary_and_initial_conditions.f90
+++ Earth_L_10_Imajo/condition_1/calculation_iteration/boundary_and_initial_conditions.f90
@@ -11,13 +11,10 @@ module boundary_and_initial_conditions
     !(initial_min_grid_1 < initial_fix_grid < initial_min_grid_2 => fix)
     !(initial_fix_grid <= initial_min_grid_1 or initial_min_grid_2 <= initial_fix_grid => not fix)
 
-    integer, parameter :: initial_min_grid_start = 112
-    integer, parameter :: initial_min_grid_end = initial_fix_grid
-
-    integer, parameter :: initial_grid_ionophere_middle_1 = 23
+    integer, parameter :: initial_grid_ionophere_middle_1 = 14
     ! 1 ~ initial_grid_ionophere_middle_1 - 1   initial_electrostatic_potential_ionosphere
     
-    integer, parameter :: initial_grid_middle_magnetosphere_1 = 112
+    integer, parameter :: initial_grid_middle_magnetosphere_1 = 109
     ! initial_grid_ionosphere_middle_1 ~ initial_grid_middle_magnetosphere_1 - 1    initial_electrostatic_potential_middle
 
     integer, parameter :: initial_grid_middle_magnetosphere_2 = real_grid_number + 2 - initial_grid_middle_magnetosphere_1
@@ -26,6 +23,9 @@ module boundary_and_initial_conditions
     integer, parameter :: initial_grid_ionophere_middle_2 = real_grid_number + 2 - initial_grid_ionophere_middle_1
     ! initial_grid_middle_magnetosphere_2 ~ initial_grid_ionophere_middle_2 - 1     initial_electrostatic_potential_middle
     ! initial_grid_ionophere_middle_2 ~ real_grid_number    initial_electrostatic_potential_ionosphere
+
+    integer, parameter :: initial_min_grid_start = initial_grid_middle_magnetosphere_1
+    integer, parameter :: initial_min_grid_end = initial_fix_grid
 
     double precision, parameter :: initial_electrostatic_potential_ionosphere = 1d4     ![V] = [kg m2 s-3 A-1]
     double precision, parameter :: initial_electrostatic_potential_middle = 8d3       ![V] = [kg m2 s-3 A-1]
