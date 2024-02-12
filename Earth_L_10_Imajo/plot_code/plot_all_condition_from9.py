b1fc72b746816f6abf2c5809834fcf69cedf6062
santanka <mkrock1062@gmail.com>
2024-02-12 19:33:16 +0900

    Case 18の計算　済

--- Earth_L_10_Imajo/plot_code/plot_all_condition_from9.py
+++ Earth_L_10_Imajo/plot_code/plot_all_condition_from9.py
@@ -7,8 +7,8 @@ grid_ionosphere_middle = 14
 grid_middle_magnetosphere = 109
 grid_fix = 175
 
-BC_number = 17
-min_number = 162
+BC_number = 18
+min_number = 113
 
 channel = 1
 
--- Earth_L_10_Imajo/plot_code/plot_number_density_iteration.py
+++ Earth_L_10_Imajo/plot_code/plot_number_density_iteration.py
@@ -7,8 +7,8 @@ grid_ionosphere_middle = 14
 grid_middle_magnetosphere = 109
 grid_fix = 175
 
-BC_number = 17
-min_number = 126
+BC_number = 18
+min_number = 174
 
 l_shell = 10E0
 series_number = 12
